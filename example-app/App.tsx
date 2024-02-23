39ee39f10c9d434c8cee7639c4f4201ddc04a29d
Antoine Cottineau <antoine.cottineau.dev@gmail.com>
2024-02-23 08:51:17 +0100

    feat: make example-app work with monorepos

--- example-app/App.tsx
+++ example-app/App.tsx
@@ -1,13 +1,11 @@
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
-import { TestComponent } from 'react-native-swipeable-card-stack'
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
-      <TestComponent />
     </View>
   )
 }
