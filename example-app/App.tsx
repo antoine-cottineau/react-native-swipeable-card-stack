2495382bd4d12d8d93cd59395c8b46ca66f2b789
Antoine Cottineau <antoine.cottineau.dev@gmail.com>
2024-02-23 08:53:15 +0100

    refactor: remove default export

--- example-app/App.tsx
+++ example-app/App.tsx
@@ -1,14 +1,12 @@
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  )
-}
+export const App = () => (
+  <View style={styles.container}>
+    <Text>Open up App.tsx to start working on your app!</Text>
+    <StatusBar style="auto" />
+  </View>
+)
 
 const styles = StyleSheet.create({
   container: {
