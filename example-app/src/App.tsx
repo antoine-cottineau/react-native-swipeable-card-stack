f1a7916619c41cffb58584c65facdb96b62fc987
Antoine Cottineau <antoine.cottineau.dev@gmail.com>
2024-03-02 18:35:37 +0100

    feat: add basic buttons at the bottom

--- example-app/src/App.tsx
+++ example-app/src/App.tsx
@@ -1,10 +1,15 @@
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
 import { SwipeableCardStack } from 'react-native-swipeable-card-stack'
 import { CatCard } from './CatCard'
+import { StackBottomOverlay } from './StackBottomOverlay'
 import { cats } from './cats'
 
 export const App = () => (
-  <GestureHandlerRootView style={{ flex: 1 }}>
-    <SwipeableCardStack data={cats} renderCard={CatCard} />
-  </GestureHandlerRootView>
+  <SafeAreaProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SwipeableCardStack data={cats} renderCard={CatCard} />
+      <StackBottomOverlay />
+    </GestureHandlerRootView>
+  </SafeAreaProvider>
 )
