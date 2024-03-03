eba5bffd3a4a4eeadd08fc31b10bf5b32f3f4566
Antoine Cottineau <antoine.cottineau.dev@gmail.com>
2024-03-03 10:31:19 +0100

    feat: animate card swipe

--- example-app/src/CatCard.tsx
+++ example-app/src/CatCard.tsx
@@ -1,4 +1,8 @@
 import styled from '@emotion/native'
+import Animated, {
+  interpolate,
+  useAnimatedStyle,
+} from 'react-native-reanimated'
 import { RenderCardProps } from 'react-native-swipeable-card-stack'
 import { Action } from './Action'
 import { CatCardBottomView } from './CatCardBottomView'
@@ -8,15 +12,40 @@ type Props = RenderCardProps<CatDataItem> & {
   onAction: (action: Action) => void
 }
 
-export const CatCard = ({ name, age, imageUrl, onAction }: Props) => (
-  <Container>
-    <FullScreenImage source={{ uri: imageUrl }} />
-    <CatCardBottomView name={name} age={age} onAction={onAction} />
-  </Container>
-)
+export const CatCard = ({
+  name,
+  age,
+  imageUrl,
+  onAction,
+  animationPosition,
+  index,
+  currentIndex,
+}: Props) => {
+  const animatedStyle = useAnimatedStyle(() => {
+    if (index !== currentIndex) {
+      return {}
+    }
+    return {
+      transform: [{ rotate: `${animationPosition.value * 10}deg` }],
+      borderRadius: interpolate(
+        Math.abs(animationPosition.value),
+        [0, 0.2],
+        [0, 16],
+      ),
+    }
+  })
 
-const Container = styled.View({
+  return (
+    <Container style={animatedStyle}>
+      <FullScreenImage source={{ uri: imageUrl }} />
+      <CatCardBottomView name={name} age={age} onAction={onAction} />
+    </Container>
+  )
+}
+
+const Container = styled(Animated.View)({
   flex: 1,
+  overflow: 'hidden',
 })
 
 const FullScreenImage = styled.Image({
