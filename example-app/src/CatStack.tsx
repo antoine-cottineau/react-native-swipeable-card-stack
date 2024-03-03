5e8b6092e42e6ec3f024a7b53583a1b3d8994dde
Antoine Cottineau <antoine.cottineau.dev@gmail.com>
2024-03-03 10:59:45 +0100

    feat: add a screen for when there is no more cards

--- example-app/src/CatStack.tsx
+++ example-app/src/CatStack.tsx
@@ -1,30 +1,45 @@
+import styled from '@emotion/native'
 import { useRef } from 'react'
 import { SwipeableCardStack } from 'react-native-swipeable-card-stack'
 import { SwipeableCardRef } from 'react-native-swipeable-card-stack/dist/SwipeableCardStack'
 import { CatCard } from './CatCard'
+import { EndOfStackView } from './EndOfStackView'
 import { cats } from './cats'
 
 export const CatStack = () => {
   const ref = useRef<SwipeableCardRef>(null)
 
   return (
-    <SwipeableCardStack
-      data={cats}
-      renderCard={(props) => (
-        <CatCard
-          {...props}
-          onAction={(action) => {
-            if (action === 'swipe-left') {
-              ref.current?.swipeLeft()
-              return
-            }
-            if (action === 'swipe-right') {
-              ref.current?.swipeRight()
-            }
-          }}
-        />
-      )}
-      ref={ref}
-    />
+    <>
+      <EndOfStackContainer>
+        <EndOfStackView />
+      </EndOfStackContainer>
+      <SwipeableCardStack
+        data={cats}
+        renderCard={(props) => (
+          <CatCard
+            {...props}
+            onAction={(action) => {
+              if (action === 'swipe-left') {
+                ref.current?.swipeLeft()
+                return
+              }
+              if (action === 'swipe-right') {
+                ref.current?.swipeRight()
+              }
+            }}
+          />
+        )}
+        ref={ref}
+      />
+    </>
   )
 }
+
+const EndOfStackContainer = styled.View({
+  position: 'absolute',
+  top: 0,
+  bottom: 0,
+  left: 0,
+  right: 0,
+})
