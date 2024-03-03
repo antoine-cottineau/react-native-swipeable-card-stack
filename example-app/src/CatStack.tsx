186d3e40b844689bfae1a3a56a9cc2cde97e2fee
Antoine Cottineau <antoine.cottineau.dev@gmail.com>
2024-03-03 15:11:46 +0100

    feat: trigger haptic feedback on swipe

--- example-app/src/CatStack.tsx
+++ example-app/src/CatStack.tsx
@@ -1,10 +1,11 @@
 import styled from '@emotion/native'
+import { impactAsync, ImpactFeedbackStyle } from 'expo-haptics'
 import { useRef } from 'react'
 import { SwipeableCardStack } from 'react-native-swipeable-card-stack'
 import { SwipeableCardRef } from 'react-native-swipeable-card-stack/dist/SwipeableCardStack'
 import { CatCard } from './CatCard'
-import { EndOfStackView } from './EndOfStackView'
 import { cats } from './cats'
+import { EndOfStackView } from './EndOfStackView'
 
 export const CatStack = () => {
   const ref = useRef<SwipeableCardRef>(null)
@@ -31,6 +32,11 @@ export const CatStack = () => {
           />
         )}
         ref={ref}
+        onActiveCardUpdate={({ phase }) => {
+          if (phase === 'below-threshold' || phase === 'above-threshold') {
+            impactAsync(ImpactFeedbackStyle.Light).catch(console.error)
+          }
+        }}
       />
     </>
   )
