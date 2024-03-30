48cc4bd130c6fcd0fcf0b58d3e998c14cbab4c39
Antoine Cottineau <antoine.cottineau.dev@gmail.com>
2024-03-30 18:09:00 +0100

    feat: add a basic HomePage

---
+++ example-app/src/shared/components/HomePage.tsx
@@ -0,0 +1,8 @@
+import { Text } from 'react-native'
+import { Page } from './Page'
+
+export const HomePage = () => (
+  <Page>
+    <Text>HomePage</Text>
+  </Page>
+)
--- example-app/src/shared/components/Navigator.tsx
+++ example-app/src/shared/components/Navigator.tsx
@@ -1,9 +1,12 @@
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
 import { MatchPage, type MatchPageParams } from '../../cats/MatchPage'
 import { SwipePage } from '../../cats/SwipePage'
+import { HomePage } from './HomePage'
 
 export type RootStackParamList = {
+  Home: undefined
   Swipe: undefined
   Match: MatchPageParams
 }
@@ -13,11 +16,12 @@ const RootStack = createNativeStackNavigator<RootStackParamList>()
 export const Navigator = () => (
   <NavigationContainer>
     <RootStack.Navigator
-      initialRouteName="Swipe"
+      initialRouteName="Home"
       screenOptions={{
         headerShown: false,
       }}
     >
+      <RootStack.Screen name="Home" component={HomePage} />
       <RootStack.Screen name="Swipe" component={SwipePage} />
       <RootStack.Screen
         name="Match"
